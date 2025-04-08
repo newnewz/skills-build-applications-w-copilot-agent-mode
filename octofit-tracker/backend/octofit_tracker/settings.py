de24585d97ea9291ff1548b1e4f2c2c7fcc16cf3
Liam Zhang <liamzhang@microsoft.com>
2025-04-08 21:24:52 +0000

    Update settings.py to use djongo as the database engine

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',  # Placeholder since we are using pymongo directly
+        'ENGINE': 'djongo',
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
